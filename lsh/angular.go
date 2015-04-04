15aaafdfb461fab42fe6963fc70d81bed94f7923
Hitoshi Harada <umi.tanuki@gmail.com>
2015-04-03 18:32:08 -0700

    Split file

--- lsh/angular.go
+++ lsh/angular.go
@@ -2,7 +2,6 @@ package lsh
 
 import (
 	"math"
-	"math/rand"
 )
 
 type Distance interface {
@@ -16,6 +15,7 @@ func (_ Angular) Distance(x, y []float32) float32 {
 	if len(x) != len(y) {
 		panic("")
 	}
+
 	var xx, yy, xy float32 = 0, 0, 0
 	for i := 0; i < len(x); i++ {
 		xx += x[i] * x[i]
@@ -30,8 +30,23 @@ func (_ Angular) Distance(x, y []float32) float32 {
 	return 2.0 // cos is 0
 }
 
-func (_ Angular) GetBitVector(vecs [][]float32, v []float32) *BitVector {
-	bv := NewBitVector(len(vecs))
+func (_ Angular) Side(x, y []float32) bool {
+	var dot float32 = 0
+	for i := 0; i < len(x); i++ {
+		dot += x[i] * y[i]
+	}
+	return dot > 0
+}
+
+func (a Angular) GetBitVector(vecs [][]float32, v []float32) *BitVector {
+	l := len(vecs)
+	bv := NewBitVector(l)
+
+	for i := 0; i < l; i++ {
+		if a.Side(vecs[i], v) {
+			bv.Set(uint(i))
+		}
+	}
 
 	return bv
 }
@@ -48,106 +63,4 @@ func (e *Euclidean) distance(x, y []float32) float32 {
 		d += (x[i] - y[i]) * (x[i] - y[i])
 	}
 	return d
-}
-
-type Indexer struct {
-	rng        *rand.Rand
-	seed       int64
-	bitsize    int
-	vecsize    int
-	distance   Distance
-	hyperplane [][]float32
-	storage    *Storage
-	lookup     map[uint64]int
-}
-
-type Storage struct {
-	hash  []int32
-	pages []Page
-}
-
-type Page [1024]uint64
-
-func NewIndexer(seed int64, bitsize int, vecsize int) *Indexer {
-	idx := &Indexer{
-		rng:      rand.New(rand.NewSource(seed)),
-		seed:     seed,
-		bitsize:  bitsize,
-		vecsize:  vecsize,
-		distance: Angular{},
-		storage:  &Storage{},
-		lookup:   map[uint64]int{},
-	}
-
-	// init hyperplane
-	idx.hyperplane = make([][]float32, bitsize, bitsize)
-	for i := 0; i < bitsize; i++ {
-		vector := make([]float32, vecsize, vecsize)
-		for j := 0; j < vecsize; j++ {
-			vector[j] = float32(idx.rng.NormFloat64())
-		}
-		idx.hyperplane[i] = vector
-	}
-
-	return idx
-}
-
-func (idx *Indexer) Add(itemid uint64, vec []float32) {
-	key := idx.distance.GetBitVector(idx.hyperplane, vec)
-	pageno, ok := idx.lookup[key.Uint64()]
-	if !ok {
-		pageno = idx.allocatePage()
-		idx.lookup[key.Uint64()] = pageno
-	}
-	idx.storage.pages[pageno].Add(itemid)
-}
-
-func (idx *Indexer) allocatePage() int {
-	n := len(idx.storage.pages)
-	idx.storage.pages = idx.storage.pages[0 : n+1]
-	return n
-}
-
-// Search searches items similar to the given vector up to limit number.
-// Currently it looks up only the same hash value.  (look for another
-// bucket is TODO)
-func (idx *Indexer) Search(vec []float32, limit int) []uint64 {
-	key := idx.distance.GetBitVector(idx.hyperplane, vec)
-	pageno, ok := idx.lookup[key.Uint64()]
-	if !ok {
-		return []uint64{}
-	}
-	page := &idx.storage.pages[pageno]
-
-	result := make([]uint64, 0, limit)
-	i := 0
-	for {
-		if len(result) == limit {
-			break
-		}
-
-		if page.Get(i) == 0 {
-			break
-		}
-		result = append(result, page.Get(i))
-		i++
-
-		// TODO: go to next page if i exceeds items in page
-	}
-
-	return result
-}
-
-func (p *Page) Add(itemid uint64) {
-	itemlen := (*p)[0] + 1
-	(*p)[int(itemlen)] = itemid
-	(*p)[0] = itemlen
-}
-
-func (p *Page) Get(n int) uint64 {
-	itemlen := (*p)[0]
-	if int(itemlen) <= n {
-		return 0
-	}
-	return (*p)[n+1]
 }
---
+++ lsh/index.go
@@ -0,0 +1,115 @@
+package lsh
+
+import(
+	"math"
+	"math/rand"
+)
+
+type Indexer struct {
+	rng        *rand.Rand
+	seed       int64
+	bitsize    int
+	vecsize    int
+	distance   Distance
+	hyperplane [][]float32
+	storage    *Storage
+	lookup     map[uint64]int
+}
+
+type Storage struct {
+	hash  []int32
+	pages []Page
+}
+
+type Page [1024]uint64
+
+func NewIndexer(seed int64, bitsize int, vecsize int) *Indexer {
+	idx := &Indexer{
+		rng:      rand.New(rand.NewSource(seed)),
+		seed:     seed,
+		bitsize:  bitsize,
+		vecsize:  vecsize,
+		distance: Angular{},
+		storage:  &Storage{},
+		lookup:   map[uint64]int{},
+	}
+
+	// init hyperplane
+	idx.hyperplane = make([][]float32, bitsize, bitsize)
+	for i := 0; i < bitsize; i++ {
+		vector := make([]float32, vecsize, vecsize)
+		var sum float64 = 0
+		for j := 0; j < vecsize; j++ {
+			vector[j] = float32(idx.rng.NormFloat64())
+			sum += float64(vector[j]) * float64(vector[j])
+		}
+		norm := float32(math.Sqrt(sum))
+		// normalize
+		for j := 0; j < vecsize; j++ {
+			vector[j] /= norm
+		}
+		idx.hyperplane[i] = vector
+	}
+
+	return idx
+}
+
+func (idx *Indexer) Add(itemid uint64, vec []float32) {
+	key := idx.distance.GetBitVector(idx.hyperplane, vec)
+	pageno, ok := idx.lookup[key.Uint64()]
+	if !ok {
+		pageno = idx.allocatePage()
+		idx.lookup[key.Uint64()] = pageno
+	}
+	idx.storage.pages[pageno].Add(itemid)
+}
+
+func (idx *Indexer) allocatePage() int {
+	n := len(idx.storage.pages)
+	idx.storage.pages = idx.storage.pages[0 : n+1]
+	return n
+}
+
+// Search searches items similar to the given vector up to limit number.
+// Currently it looks up only the same hash value.  (look for another
+// bucket is TODO)
+func (idx *Indexer) Search(vec []float32, limit int) []uint64 {
+	key := idx.distance.GetBitVector(idx.hyperplane, vec)
+	pageno, ok := idx.lookup[key.Uint64()]
+	if !ok {
+		return []uint64{}
+	}
+	page := &idx.storage.pages[pageno]
+
+	result := make([]uint64, 0, limit)
+	i := 0
+	for {
+		if len(result) == limit {
+			break
+		}
+
+		if page.Get(i) == 0 {
+			break
+		}
+		result = append(result, page.Get(i))
+		i++
+
+		// TODO: go to next page if i exceeds items in page
+	}
+
+	return result
+}
+
+func (p *Page) Add(itemid uint64) {
+	itemlen := (*p)[0] + 1
+	(*p)[int(itemlen)] = itemid
+	(*p)[0] = itemlen
+}
+
+func (p *Page) Get(n int) uint64 {
+	itemlen := (*p)[0]
+	if int(itemlen) <= n {
+		return 0
+	}
+	return (*p)[n+1]
+}
